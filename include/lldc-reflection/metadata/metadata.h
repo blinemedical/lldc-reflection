3662914bc4c14ad5dc9baaf5bd5447b48bd87fad
Thomas Goodwin <thomas.goodwin@laerdal.com>
2023-05-22 13:31:19 -0400

    metadata: clarifying comments on the behavior of these flags
    Signed-off-by: Thomas Goodwin <thomas.goodwin@laerdal.com>

--- include/lldc-reflection/metadata/metadata.h
+++ include/lldc-reflection/metadata/metadata.h
@@ -13,12 +13,30 @@
 namespace lldc::reflection::metadata {
   /**
    * @brief Properties marked as optional will have this metadata set in their RTTR registration.
+   * "To" Behavior:
+   *   Container types (e.g., string, vector) are skipped if marked optional and are empty/zero-length.
+   *   Pointer types are skipped if set to null (i.e., empty).
+   *   Everything else will be included since without a default value there is no unambiguous way to
+   *     know _when_ it is optional.
+   *
+   * "From" Behavior:
+   *   If the property member name is not found, and it has been marked optional, it will be skipped
+   *   without trigging a failure of serialization.  The expectation is the target RTTR-registered
+   *   type will already have a default set for that member, so serialization should not touch it.
    */
   LLDC_REFLECTION_API
   ::rttr::detail::metadata set_is_optional();
 
   /**
    * @brief Treat the property as optional if this value is set.
+   * "To" Behavior:
+   *   When preparing to convert the member, if the source value matches the declared default (via
+   *     operator==), the member will be skipped.
+   *
+   * "From" Behavior:
+   *   If the property member name is not found, and it has been marked optional, it will be skipped
+   *   without trigging a failure of serialization.  The expectation is the target RTTR-registered
+   *   type will already have a default set for that member, so serialization should not touch it.
    */
   LLDC_REFLECTION_API
   ::rttr::detail::metadata set_is_optional_with_default(::rttr::variant value);
@@ -26,12 +44,18 @@ namespace lldc::reflection::metadata {
   /**
    * @brief Properties that should not be serialized will have this metadata set in their RTTR
    * registrations.
+   *
+   * NOTE: This is the same as simply not declaring the property/member in the
+   *   RTTR[_PLUGIN]_REGISTRATION macro, but provided in the event one is trying to provide some
+   *   bookkeeping or otherwise pertaining to the member.
    */
   LLDC_REFLECTION_API
   ::rttr::detail::metadata set_is_do_not_serialize();
 
   /**
-   * @brief Property will be stored/recovered as a character string.
+   * @brief Property will be stored/recovered as a character string.  This is a way to provide a
+   * generic "payload" member in an object which can then be passed to another to/from routine
+   * with other registered types pertianing to payload(s) of the parent class.
    */
   LLDC_REFLECTION_API
   ::rttr::detail::metadata set_is_blob();
